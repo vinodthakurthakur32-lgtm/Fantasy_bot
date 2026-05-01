a6d80bb5acb37d574d8ebcd8bf1e1afd94750d0a
vinodthakurthakur32-lgtm <vinodthakurthakur32@gmail.com>
2026-05-01 16:19:25 +0530

    Update admin_app.py

--- admin_app.py
+++ admin_app.py
@@ -126,4 +126,4 @@ def handle_admin_nav(call, bot):
     elif nav == "adm_nav_help":
         bot.answer_callback_query(call.id)
         markup, text = ui.admin_help_render()
-        bot.edit_message_text(text, chat_id, mid, reply_markup=markup, parse_mode='HTML')
+        bot.edit_message_text(text, chat_id, mid, reply_markup=markup, parse_mode='HTML', disable_web_page_preview=True)
